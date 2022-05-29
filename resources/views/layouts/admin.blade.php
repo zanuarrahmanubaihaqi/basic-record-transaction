6a882ecaf3fa7b31de163a0cec74755866f31903
baihaqi <zanu.pencak@gmail.com>
2022-05-29 13:52:07 +0700

    update menu

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -79,7 +79,7 @@
           <div id="report" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
               <a class="collapse-item" href="{{ route('orderreport.index') }}">Data Report</a>
-              <a class="collapse-item" href="{{ route('orderreport.index') }}">Data Report Filtered</a>
+              {{-- <a class="collapse-item" href="{{ route('orderreport.index') }}">Data Report Filtered</a> --}}
             </div>
           </div>
         </li>
